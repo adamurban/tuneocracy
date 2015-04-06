efba4764fef85db56b6889553854fa10d0723eda
Adam Urban <adam.urban@philips.com>
2015-04-06 01:20:08 -0400

    Remove hardcoded php path

--- admin/start_playback_engine.php
+++ admin/start_playback_engine.php
@@ -1,4 +1,7 @@
 <?php
+
+$full_php_path = PHP_BINDIR . "/php";
+
 $lock_file_name = 'playback_lock';
 $full_lock_path = getcwd() . "/" . $lock_file_name;
 
@@ -6,7 +9,7 @@ if (file_exists($full_lock_path)) {
     $pid = file_get_contents($lock_file_name);
     echo "Playback Engine already running with PID " . $pid;
 } else {
-    $pid = exec('nohup /Applications/MAMP/bin/php/php5.6.2/bin/php play_songs.php > /dev/null 2>&1 & echo $!');
+    $pid = exec("nohup $full_php_path play_songs.php > /dev/null 2>&1 & echo $!");
     file_put_contents($lock_file_name, $pid);
     echo "Started Playback Engine, PID " . $pid;
 }
